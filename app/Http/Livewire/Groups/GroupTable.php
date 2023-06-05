ae2704dbf3d2352cd244cd12e59f0dbaa642343e
Caique Bispo <caiquebispo0023@gmail.com>
2023-06-05 09:08:58 -0300

    Finaliando Funcionalidade de Grupos

--- app/Http/Livewire/Groups/GroupTable.php
+++ app/Http/Livewire/Groups/GroupTable.php
@@ -5,17 +5,24 @@ namespace App\Http\Livewire\Groups;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Group;
-use App\Models\User;
 use DateTime;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 
 class GroupTable extends DataTableComponent
 {
+    public $selectedItems;
+    public $canShowModal = false;
+    public  ?string $name;
 
+    protected $rules = [
+
+        'name' => 'required|min:4|max:150'
+    ];
     protected $listeners = [
         'groups::index::created' => '$refresh',
         'groups::index::delete' => '$refresh',
+        'groups::index::updated' => '$refresh',
     ];
 
     public array $bulkActions = [
@@ -56,9 +63,26 @@ class GroupTable extends DataTableComponent
     }
     public function deleteSelected()
     {
-        foreach ($this->getSelected() as $item) {
-            Group::query()->where('company_id', Auth::user()->company_id)->where('id', $item)->delete();
-        }
+        Group::query()->where('company_id', Auth::user()->company_id)->whereIn('id', $this->getSelected())->delete();
         $this->clearSelected();
+    }
+    public function updateSelected()
+    {   
+        $this->selectedItems = Group::query()->where('company_id', Auth::user()->company_id)->whereIn('id', $this->getSelected())->first();
+        $this->canShowModal = true;
+        $this->emit('canShowModal', "livewire.groups.update", ['selectedItems' => $this->selectedItems]);
+        
+    }
+    public function customView(): string
+    {
+        return 'livewire.groups.update';
+    }
+
+    public function update(){
+
+        $validated = $this->validate();
+        Group::query()->where('company_id', Auth::user()->company_id)->whereIn('id', $this->getSelected())->update($validated);
+        $this->clearSelected();
+        $this->canShowModal = false;
     }
 }
