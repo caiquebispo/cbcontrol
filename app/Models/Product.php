19eabf1915d97350122fd10740ea0c4442c39fd6
Caique Bispo <caiquebispo0023@gmail.com>
2023-06-24 09:21:31 -0300

    Criando Componente de listagem de produto

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function categories(): HasOne
+    {
+        return $this->hasOne(Category::class);
+    }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,7 +3,7 @@
         <div class="container mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class=" p-6 text-gray-900 dark:text-gray-100 shadow-lg">
-                    {{ __("Products") }}
+                    <livewire:products.list-products />
                 </div>
             </div>
         </div>
