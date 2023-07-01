2eccaefc734208136d7d45f674c4cd8d5a20fc6f
Caique Bispo <caiquebispo0023@gmail.com>
2023-07-01 19:06:04 -0300

    Criando Compoente pra cadastro de endereco da empresa

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -2,13 +2,35 @@
     <div class="py-24">
         <div class="container mx-auto sm:px-6 lg:px-8">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <header class="mb-5">
+                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                        {{ __('Upload de Imagens') }}
+                    </h2>
+                </header>
                 <div class="max-w-xl">
                     <livewire:companies.upload-photo />
                 </div>
             </div>
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg my-5">
+               <header class="mb-5">
+                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                        {{ __('Informações da empresa') }}
+                    </h2>
+                </header>
                 <div>
                     <livewire:companies.update/>
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg my-5">
+               <header class="mb-5">
+                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 flex justify-between">
+                        {{ __('Informações de Endereço') }}
+                        <x-button icon="pencil" primary label="CADASTRAR ENDEREÇO"  onclick="Livewire.emit('openModal', 'companies.create-address')" class="my-2"/>
+                    </h2>
+                    <livewire:companies.update-address />
+                </header>
+                <div>
+                   
                 </div>
             </div>
         </div>
---
+++ resources/views/livewire/companies/update-address.blade.php
@@ -0,0 +1,36 @@
+<div>
+    <x-errors />
+        <form wire:submit.prevent="update" class="my-2">
+            @csrf
+            <div class="grid md:grid-cols-3 md:gap-6 my-3">
+                <div>
+                    <x-input label="Estado" placeholder="Estado" wire:model.defer="address.states"/>
+                </div>
+                <div>
+                    <x-input label="CEP" placeholder="CEP" wire:model.defer="address.zipe_code" />
+                </div>
+                <div>
+                    <x-input label="Cidade"  placeholder="Cidade" wire:model.defer="address.city" />
+                </div>
+            </div>
+            <div class="grid md:grid-cols-3 md:gap-6 my-3">
+                <div>
+                    <x-input label="Bairro" placeholder="Bairro" wire:model.defer="address.neighborhood"/>
+                </div>
+                <div>
+                    <x-input label="Rua" placeholder="Rua" wire:model.defer="address.road" />
+                </div>
+                <div>
+                    <x-input label="Nª Da Casa"  placeholder="Nª Da Casa" wire:model.defer="address.number" />
+                </div>
+            </div>
+            <div class="grid md:grid-cols-1 md:gap-6 my-3">
+               
+                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Complemento</label>
+                <textarea wire:model.defer="address.complement" id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""></textarea>
+
+            </div>
+            
+            <x-button type="submit" icon="pencil" primary label="ATUALIZAR" class="my-2"/>
+        </form>
+</div>
