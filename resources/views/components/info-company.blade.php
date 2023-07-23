66c5826d41273685372b027353537a09292c8c0a
Caique Bispo <caiquebispo0023@gmail.com>
2023-07-23 10:29:14 -0300

    Implementando compoente de forma de retirada

--- resources/views/components/info-company.blade.php
+++ resources/views/components/info-company.blade.php
@@ -1,7 +1,5 @@
 <div class="w-full content-info-compan flex flex-col items-center">
-
     <x-company-image :path="auth()->user()->company->image()->value('path')" />
-
     <p class="w-full bg-gray-900 dark:bg-gray-950 rounded-lg p-1 text-md font-semibold text-gray-300 text-center uppercase my-3">
         {{auth()->user()->company->corporate_reason}}
     </p>
--- resources/views/livewire/store/checkout/checkout.blade.php
+++ resources/views/livewire/store/checkout/checkout.blade.php
@@ -6,7 +6,7 @@
             <hr class="mb-4">
         </div>
         <x-resume-cart :items="\Cart::content()" :value="\Cart::subtotal()" :delivery_price="$company->settings->delivery_price"/>
-
+        <x-form-of-payment :has_delivery="$company->settings->has_delivery" :has_limit_price_delivery="$company->settings->has_limit_price_delivery" :value="\Cart::subtotal()" />
     @else
         <h3>Não está logado</h3>
         <x-resume-cart :items="\Cart::content()" :value="\Cart::subtotal()" :delivery_price="$company->settings->delivery_price" />
