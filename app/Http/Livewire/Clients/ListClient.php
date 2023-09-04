2c21d444f718ec57ab630e915d2521a36ee36d16
Caique Bispo <caiquebispo0023@gmail.com>
2023-09-04 16:03:15 -0300

    Adicionando paginação no component

---
+++ app/Http/Livewire/Clients/ListClient.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Livewire\Clients;
+
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ListClient extends Component
+{
+    use WithPagination;
+    public ?string $search = '';
+    public User $user;
+    protected $listeners = ['client::index::created' => '$refresh'];
+    public function __construct()
+    {
+        $this->user = Auth::user();
+    }
+
+    public function render(): View
+    {
+        return view('livewire.clients.list-client', ['clients' => $this->getClients()]);
+    }
+    protected function getClients(){
+
+        return $this->user->company->clients()
+            ->when($this->search != "", fn($query) => $query->where('full_name', 'like', '%'.$this->search."%"))
+            ->paginate(10);
+    }
+}
---
+++ resources/views/livewire/clients/list-client.blade.php
@@ -0,0 +1,47 @@
+<div>
+    <div class="container mx-auto sm:px-6 lg:px-8">
+        <x-table.content-table>
+            <x-table.actions >
+                <x-slot name="buttonCreate">
+                    <livewire:clients.create />
+                </x-slot>
+            </x-table.actions>
+            <x-table>
+                <x-table.thead>
+                    <x-table.th>NOME</x-table.th>
+                    <x-table.th class="text-center">GRUPO</x-table.th>
+                    <x-table.th class="text-center">TELEFONE</x-table.th>
+                    <x-table.th class="text-center">VALOR</x-table.th>
+                    <x-table.th class="text-center">LOCAL</x-table.th>
+                    <x-table.th class="text-center">PAGAMENTO</x-table.th>
+                    <x-table.th class="text-center">ENTREGA</x-table.th>
+                    <x-table.th class="text-right">ACTIONS</x-table.th>
+                </x-table.thead>
+                <x-table.tbody>
+                    @foreach($clients as $client)
+                        <x-table.tr>
+                            <x-table.th>{{$client->full_name ?? 'NÃO DEFINIDO'}}</x-table.th>
+                            <x-table.th class="text-center">{{$client->groups->value('name') ?? 'NÃO DEFINIDO'}}</x-table.th>
+                            <x-table.th class="text-center">{{$client->number_phone ?? 'NÃO DEFINIDO'}}</x-table.th>
+                            <x-table.th class="text-center">R$ {{ number_format($client->value, 2, ',','.')}}</x-table.th>
+                            <x-table.th class="text-center">{{$client->local ?? 'NÃO DEFINIDO'}}</x-table.th>
+                            <x-table.th class="text-center">{{$client->payment_method ?? 'NÃO DEFINIDO'}}</x-table.th>
+                            <x-table.th class="text-center">{{$client->delivery ?? 'NÃO DEFINIDO'}}</x-table.th>
+                            <x-table.th class="flex items-center justify-end">
+                                <x-table.td-actions>
+                                    <x-table.td-action-li>
+{{--                                        <livewire:clients.create />--}}
+                                    </x-table.td-action-li>
+                                    <x-table.td-action-li>Excluir - <x-button-trash /></x-table.td-action-li>
+                                </x-table.td-actions>
+                            </x-table.th>
+                        </x-table.tr>
+                    @endforeach
+                </x-table.tbody>
+            </x-table>
+            <div class="p-4">
+                {{ $clients->links() }}
+            </div>
+        </x-table.content-table>
+    </div>
+</div>
