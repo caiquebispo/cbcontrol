006e33b81974bdab22527784a0f49f5fe93ba595
Caique Bispo <caiquebispo0023@gmail.com>
2023-09-25 19:43:54 -0300

    Criando os relacionamento entre usuário e rede, e entre rede e empresa

--- app/Models/Network.php
+++ app/Models/Network.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Network extends Model
 {
     use HasFactory;
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'network_users');
+    }
+    public function companies(): BelongsToMany
+    {
+        return $this->belongsToMany(Company::class, 'network_companies');
+    }
 }
