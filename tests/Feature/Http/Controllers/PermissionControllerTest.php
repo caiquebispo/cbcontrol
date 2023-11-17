3e7df842247fdc0dd6c1b038ce6c6255f10b61d6
Caique Bispo <caiquebispo0023@gmail.com>
2023-11-17 09:13:39 -0300

    test(permission): criando os todo básico para criação do perfil

---
+++ tests/Feature/Http/Controllers/PermissionControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+//Profiles Tests
+
+todo('verificar se existe a rota permissions');
+todo('verificar se somente usuários autenticado podem acessar essa rota');
+todo('verificar se usuários não autenticados estão sendo redirecionando para pagina de login');
+todo('verificar se na pagina tem o componente para criação de um perfil');
+todo('verificar se ao clicar no componente o modal para cadastro de perfil está sendo exibido corretamente');
+todo('verificar se os dados estão devidamente preenchidos');
+todo('verificar se foi realizado o cadastro de um perfil com sucesso e se o evento foi desparado corretamente');
+
+//Permission "Modules" Tests
+
