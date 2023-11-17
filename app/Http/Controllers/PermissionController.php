1d3c1926414c8b4e20fe25f65bf691789c9963a4
Caique Bispo <caiquebispo0023@gmail.com>
2023-11-17 09:40:13 -0300

    test(permission): verificar se somente usuários autenticado podem acessar essa rota

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
-    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(): View
     {
          return view('permissions.index');
--- tests/Feature/Http/Controllers/PermissionControllerTest.php
+++ tests/Feature/Http/Controllers/PermissionControllerTest.php
@@ -2,7 +2,13 @@
 
 //Profiles Tests
 
-use function Pest\Laravel\{get,};
+use App\Models\User;
+
+use function Pest\Laravel\{actingAs, get,};
+beforeEach(function(){
+    
+    $this->user = User::factory()->createOne();
+});
 
 it('verificar se existe a rota permissions', function(){
 
@@ -10,7 +16,13 @@ it('verificar se existe a rota permissions', function(){
     ->assertOk()
     ->assertViewIs('permissions.index');
 });
-todo('verificar se somente usuários autenticado podem acessar essa rota');
+it('verificar se somente usuários autenticado podem acessar essa rota', function(){
+
+    actingAs($this->user)
+    ->get('/app/permissions')
+    ->assertOk()
+    ->assertViewIs('permissions.index');
+});
 todo('verificar se usuários não autenticados estão sendo redirecionando para pagina de login');
 todo('verificar se na pagina tem o componente para criação de um perfil');
 todo('verificar se ao clicar no componente o modal para cadastro de perfil está sendo exibido corretamente');
