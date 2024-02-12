add4488802a3fac668a6e2addc79267d457ae1ef
Caique Bispo <caiquebispo0023@gmail.com>
2024-02-12 11:57:26 -0300

    feat(report) Adionando um select search no módulo de boxfront

--- routes/web/app/clients.php
+++ routes/web/app/clients.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\ClientController;
 Route::get('clients', [ClientController::class, 'index']);
 Route::get('clients/ultragaz', [ClientController::class, 'ultragaz']);
 Route::get('clients/exportPDF', [ClientController::class, 'exportPDF']);
+Route::get('clients/getAll', [ClientController::class, 'getAll'])->name('client.getAll');
