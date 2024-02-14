be173f9430d9bddf1a50b2320fe64ed9077699c3
Caique Bispo <caiquebispo0023@gmail.com>
2024-02-14 20:49:38 -0300

    feat(report): finalizando scrip para ler os relatório de cliente

--- app/Imports/ClientsImport.php
+++ app/Imports/ClientsImport.php
@@ -2,8 +2,10 @@
 
 namespace App\Imports;
 
-use App\Models\Client;
+use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
 class ClientsImport implements ToModel
 {
@@ -14,9 +16,29 @@ class ClientsImport implements ToModel
      */
     public function model(array $row)
     {
-        dd($row);
-        return new Client([
-            //
-        ]);
+        set_time_limit(0);
+        $company = Auth::user()->company;
+
+        if ($row[0] != 'NOME') {
+
+            if (!$company->clients()->where('full_name', strtoupper($row[0]))->first()) {
+
+                $company->clients()
+                    ->create([
+
+                        'full_name' => strtoupper($row[0]),
+                        'group_id' => 10,
+
+                    ])->address()->create([
+
+                        'states' => 'BA',
+                        'zipe_code' => 45500000,
+                        'city' => 'Ibirapitanga',
+                        'neighborhood' => $row[1] != "" ? $row[1] : 'N/D',
+                        'road' => $row[2] != "" ? $row[2] : 'N/D',
+
+                    ]);
+            }
+        }
     }
 }
--- resources/views/livewire/report/upload.blade.php
+++ resources/views/livewire/report/upload.blade.php
@@ -1,10 +1,13 @@
 <div>
+    <div x-data="{ isUploading: false, progress: 0 }" x-on:livewire-upload-start="isUploading = true" x-on:livewire-upload-finish="isUploading = false" x-on:livewire-upload-error="isUploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress">
+        <div x-show="isUploading">
+            <progress max="100" x-bind:value="progress"></progress>
+        </div>
+        <form wire:submit.prevent="upload">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" wire:model="file" type="file">
+            <button type="submit">Upload</button>
+        </form>
 
-    <form wire:submit.prevent="upload">
-        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
-        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" wire:model="file" type="file">
-        <button type="submit">Upload</button>
-    </form>
 
-
-</div>
+    </div>
