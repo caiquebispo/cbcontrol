e30dc63cab86c88aa78320ae6592214745b86523
Caique Bispo <caiquebispo0023@gmail.com>
2024-02-18 08:51:48 -0300

    feat(order-hav): criando model e migration

---
+++ app/Models/OrderHav.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderHav extends Model
+{
+    use HasFactory;
+}
